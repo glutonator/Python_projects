c03021bc01a0574b4684fac6942f4c46901679b9
glutonator <glutonator@gmail.com>
2017-11-05 00:16:46 +0100

    zad6

---
+++ zadanie_domowe_2/zadanie6.py
@@ -0,0 +1,44 @@
+
+def ewakuacja (lista_osob, liczba_klatek_schodowych, liczba_osob_w_rzedzie, tempo_schodzenia ):
+    odst_czas = 1
+    out = list()
+    temp = liczba_osob_w_rzedzie * liczba_klatek_schodowych
+    lista_osob_rev = list(lista_osob)
+    lista_osob_rev.reverse()
+    czas_od_pocz =0    
+    for x in lista_osob_rev:
+        out.append(czas_od_pocz)        
+        liczba_krokow_do_opusz_pietra =0
+        #liczba krokow potrzbna do opuszczenia pietra
+        for y in range(0,x,temp):
+            liczba_krokow_do_opusz_pietra+=1
+            print(y)
+        czas_od_pocz+=liczba_krokow_do_opusz_pietra*odst_czas+tempo_schodzenia
+        print("next", liczba_krokow_do_opusz_pietra)
+    out.reverse()
+    print(out)
+    return out
+    
+
+
+
+
+
+lista_osob = [5, 10, 15]
+liczba_klatek_schodowych = 2
+liczba_osob_w_rzedzie = 1
+tempo_schodzenia = 30
+
+ewakuacja(
+    lista_osob=lista_osob,
+    liczba_klatek_schodowych=liczba_klatek_schodowych,
+    liczba_osob_w_rzedzie=liczba_osob_w_rzedzie,
+    tempo_schodzenia=tempo_schodzenia
+)
+
+assert [73, 38, 0] == ewakuacja(
+    lista_osob=lista_osob,
+    liczba_klatek_schodowych=liczba_klatek_schodowych,
+    liczba_osob_w_rzedzie=liczba_osob_w_rzedzie,
+    tempo_schodzenia=tempo_schodzenia
+)
